265831a39b89facb7869a0edcafebfbef1896d9d
root <petterson.santos@outlook.com.br>
2022-07-13 12:03:44 -0300

    change the access route

--- launcher/src/main/java/com/example/configuration/WebSecurityConfig.java
+++ launcher/src/main/java/com/example/configuration/WebSecurityConfig.java
@@ -50,7 +50,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements W
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf().disable()
-                .authorizeRequests().antMatchers("/authenticate", "/customer/add").permitAll().
+                .authorizeRequests().antMatchers("/authenticate", "/user/add").permitAll().
                         anyRequest().authenticated().and().
                         exceptionHandling()
                 .authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
